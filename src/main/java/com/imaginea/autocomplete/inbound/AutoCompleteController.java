198d7a2d66d6d1b8d33c77a9df0d941684e381b7
Mrinal Bhattacharya <bmrinal@ford.com>
2018-09-05 14:48:45 +0530

    Add rest documention

--- src/main/java/com/imaginea/autocomplete/inbound/AutoCompleteController.java
+++ src/main/java/com/imaginea/autocomplete/inbound/AutoCompleteController.java
@@ -11,16 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.imaginea.autocomplete.service.AutoCompleteService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @Validated
+@Api(value = "AutoComplete", description = "Based on city prefix, List all the cities.")
 public class AutoCompleteController {
 
 	@Autowired
 	private AutoCompleteService autoCompleteService;
 
 	@GetMapping("/suggest_cities")
+	@ApiOperation(value = "Suggest a list of cities", response = String.class)
 	public String getCities(
-			@RequestParam(name = "start", required = true) @Valid @Pattern(message="Start should not contains special character" ,regexp = "[a-zA-Z0-9 ]+") String prefix,
+			@RequestParam(name = "start", required = true) @Valid @Pattern(message = "Start should not contains special character", regexp = "[a-zA-Z0-9 ]+") String prefix,
 			@RequestParam(name = "atmost", required = false, defaultValue = Integer.MAX_VALUE + "") int maxSize) {
 		return this.autoCompleteService.getCities(prefix, maxSize);
 	}
